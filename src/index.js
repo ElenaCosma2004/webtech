b7ebf054d0cfa6dbad4a81358a68eeabf9ba3571
ElenaCosma2004 <elenacosma2004@gmail.com>
2025-10-05 11:09:14 +0000

    added hello.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello from CodeSandbox!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080, () => {
+    console.log("Server running at http://localhost:8080/");
+  });
